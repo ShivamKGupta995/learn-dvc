0b94c2af35d9b11c0d5895b5719e0d6fa7da1417
Shivam8552051878 <sg9967780426@gmail.com>
2025-06-24 10:08:29 +0530

    version 1 of data

---
+++ src/data_ingestion.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+import numpy as np
+
+df = pd.read_csv("https://raw.githubusercontent.com/araj2/customer-database/refs/heads/master/Ecommerce%20Customers.csv")
+print(df.iloc[0])
+
+df = df.drop(columns=["Email", "Address", "Avatar"])
+df = df[df["Length of Membership"] > 3]
+print(df.shape)
+
+df.to_csv(os.path.join("data", "cleaned_data.csv"), index=False)
