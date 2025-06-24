cb43a7d5bce8573766982f473dedb8a116d59ca6
Shivam8552051878 <sg9967780426@gmail.com>
2025-06-24 10:16:18 +0530

    add second expo

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -5,8 +5,8 @@ import numpy as np
 df = pd.read_csv("https://raw.githubusercontent.com/araj2/customer-database/refs/heads/master/Ecommerce%20Customers.csv")
 print(df.iloc[0])
 
-df = df.drop(columns=["Email", "Address", "Avatar"])
-df = df[df["Length of Membership"] > 3]
+df = df.drop(columns=["Email", "Address", "Avatar", "Avg. Session Length"])
+df = df[df["Length of Membership"] > 1]
 print(df.shape)
 
 df.to_csv(os.path.join("data", "cleaned_data.csv"), index=False)
